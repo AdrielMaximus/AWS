915f9a29058a8bd29d5535d8ec3550de0b706b7c
Adriel Maximus <adrvlogs885@gmail.com>
2025-03-19 15:38:27 -0300

    new files

---
+++ Bucketlist/amplify-shopping-list/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyshoppinglist": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyshoppinglist06c1fa92": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- Bucketlist/amplify-shopping-list/src/main.js
+++ Bucketlist/amplify-shopping-list/src/main.js
@@ -2,4 +2,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue3";
+import awsconfig from "./aws-exports";
+
+import BootstrapVue from "bootstrap-vue";
+import { BootstrapVueIcons } from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Amplify.configure(awsconfig);
+Vue.use(AmplifyPlugin, AmplifyModules);
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+
+
 createApp(App).use(router).mount("#app");
