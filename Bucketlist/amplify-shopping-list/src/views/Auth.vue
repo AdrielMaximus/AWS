3d8d1baf146c4a4c38c503860b4b7df05704e12b
Adriel Maximus <adrvlogs885@gmail.com>
2025-03-19 20:18:26 -0300

    test

---
+++ Bucketlist/amplify-shopping-list/src/views/Auth.vue
@@ -0,0 +1,17 @@
+<template>
+  <amplify-authenticator></amplify-authenticator>
+</template>
+
+<script>
+import { AmplifyAuthenticator } from '@aws-amplify/ui-vue';
+import { Auth } from 'aws-amplify';
+
+export default {
+  mounted(){
+    Auth.currentAuthenticatedUser()
+      .then(user => console.log({ user }))
+      .catch(() => console.log('Not signed in'));
+  }
+};
+</script>
+
